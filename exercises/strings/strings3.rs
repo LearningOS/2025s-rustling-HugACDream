3e9cb437f65b1413491815a70d038b6e6ab1a809
好饭难拼 <t202510009995203@eduxiji.net>
2025-04-24 14:48:30 +0800

    4.24

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut tmp_str = String::from(input);
+    tmp_str.push_str(" world!");
+    tmp_str
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let tmp_str = String::from(input);
+    tmp_str.replace("cars", "balloons")
 }
 
 #[cfg(test)]
